94920907875bce111edee539fe97de4d9a8b6499
sudharshan somise <somisettyssn@gmail.com>
2024-06-28 13:06:22 +0000

    All

---
+++ manage-products/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["manageproducts/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ manage-products/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"manage-products/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
